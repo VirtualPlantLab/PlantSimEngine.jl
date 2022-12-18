9f8469ab8c1169c9ccbd4f59e8b827736c791d94
Rémi Vezy <VEZY@users.noreply.github.com>
2022-12-18 12:27:02 +0100

    Set-up jldoctests in CI
    So now all docstrings with examples marked as jldoctests will be tested when testing the package.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,39 +1,52 @@
 using PlantSimEngine
 using Test, Aqua
 using Tables, DataFrames
-using PlantSimEngine.MultiScaleTreeGraph
-using PlantSimEngine.PlantMeteo
-
-Aqua.test_all(
-    PlantSimEngine,
-    # Removing this test as dependencies return ambiguities...
-    #! But do it sometimes just to check that there are no ambiguities!
-    ambiguities=false
-)
+using MultiScaleTreeGraph
+using PlantMeteo
+using Documenter # for doctests
 
 # Include the example dummy processes:
 include("../examples/dummy.jl")
 
-@testset "ModelList" begin
-    include("test-ModelList.jl")
-end
 
-@testset "Status" begin
-    include("test-Status.jl")
-end
+@testset "Testing PlantSimEngine" begin
 
-@testset "TimeStepTable" begin
-    include("test-TimeStepTable.jl")
-end
+    Aqua.test_all(
+        PlantSimEngine,
+        # Removing this test as dependencies return ambiguities...
+        #! But do it sometimes just to check that there are no ambiguities!
+        ambiguities=false
+    )
 
-@testset "Dimensions" begin
-    include("test-dimensions.jl")
-end
+    @testset "ModelList" begin
+        include("test-ModelList.jl")
+    end
 
-@testset "Simulations" begin
-    include("test-simulation.jl")
-end
+    @testset "Status" begin
+        include("test-Status.jl")
+    end
 
-@testset "Statistics" begin
-    include("test-statistics.jl")
+    @testset "TimeStepTable" begin
+        include("test-TimeStepTable.jl")
+    end
+
+    @testset "Dimensions" begin
+        include("test-dimensions.jl")
+    end
+
+    @testset "Simulations" begin
+        include("test-simulation.jl")
+    end
+
+    @testset "Statistics" begin
+        include("test-statistics.jl")
+    end
+
+    @testset "Doctests" begin
+        DocMeta.setdocmeta!(PlantSimEngine, :DocTestSetup, :(using PlantSimEngine, PlantMeteo, DataFrames); recursive=true)
+
+        # Testing the doctests, i.e. the examples in the docstrings marked with jldoctest:
+        doctest(PlantSimEngine; manual=false)
+    end
+
 end
