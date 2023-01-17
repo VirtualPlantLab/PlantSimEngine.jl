d30f119e8bb16c70c1dec2dae14f61c0046e837b
Rémi Vezy <VEZY@users.noreply.github.com>
2023-01-17 10:55:20 +0100

    Add status for MTG nodes

--- src/mtg/mtg_helpers.jl
+++ src/mtg/mtg_helpers.jl
@@ -1,3 +1,13 @@
+# status on an mtg node returns the status of the models.
+function status(node::T, key) where {T<:MultiScaleTreeGraph.NodeMTG}
+    status(node[:models], key)
+end
+
+function status(node::T) where {T<:MultiScaleTreeGraph.NodeMTG}
+    status(node[:models])
+end
+
+
 """
     pull_status!(node)
 
