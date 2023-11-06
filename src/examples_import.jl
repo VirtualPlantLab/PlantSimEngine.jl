0f776a1509c833be40f572d2fd5457d879b5f3a3
Rémi Vezy <VEZY@users.noreply.github.com>
2023-11-06 15:42:08 +0100

    Update examples_import.jl
    Add example mtg

--- src/examples_import.jl
+++ src/examples_import.jl
@@ -24,3 +24,39 @@ function import_multiscale_example()
     include(joinpath(@__DIR__, "../examples/ToyCAllocationModel.jl"))
     include(joinpath(@__DIR__, "../examples/ToySoilModel.jl"))
 end
+
+
+
+"""
+    import_mtg_example()
+
+Returns an example multiscale tree graph (MTG) with a scene, a soil, and a plant with two internodes and two leaves.
+
+# Examples
+
+```jldoctest mylabel
+julia> using PlantSimEngine
+```
+
+```jldoctest
+julia> PlantSimEngine.import_mtg_example()
+/ 1: Scene
+├─ / 2: Soil
+└─ + 3: Plant
+   └─ / 4: Internode
+      ├─ + 5: Leaf
+      └─ < 6: Internode
+         └─ + 7: Leaf
+```
+"""
+function import_mtg_example()
+    mtg = MultiScaleTreeGraph.Node(MultiScaleTreeGraph.NodeMTG("/", "Scene", 1, 0))
+    MultiScaleTreeGraph.Node(mtg, MultiScaleTreeGraph.NodeMTG("/", "Soil", 1, 1))
+    plant = MultiScaleTreeGraph.Node(mtg, MultiScaleTreeGraph.NodeMTG("+", "Plant", 1, 1))
+    internode1 = MultiScaleTreeGraph.Node(plant, MultiScaleTreeGraph.NodeMTG("/", "Internode", 1, 2))
+    MultiScaleTreeGraph.Node(internode1, MultiScaleTreeGraph.NodeMTG("+", "Leaf", 1, 2))
+    internode2 = MultiScaleTreeGraph.Node(internode1, MultiScaleTreeGraph.NodeMTG("<", "Internode", 1, 2))
+    MultiScaleTreeGraph.Node(internode2, MultiScaleTreeGraph.NodeMTG("+", "Leaf", 1, 2))
+
+    return mtg
+end
