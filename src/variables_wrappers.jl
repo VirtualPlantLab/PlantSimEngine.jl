111dc0c5ff59877d4d6a447af0d3fea6af44bf7a
Rémi Vezy <VEZY@users.noreply.github.com>
2024-04-05 11:38:02 +0200

    Create variables_wrappers.jl

---
+++ src/variables_wrappers.jl
@@ -0,0 +1,23 @@
+""" 
+    UninitializedVar(variable, value)
+
+A variable that is not initialized yet, it is given a name and a default value.
+"""
+struct UninitializedVar{T}
+    variable::Symbol
+    value::T
+end
+
+Base.eltype(u::UninitializedVar{T}) where {T} = T
+
+"""
+    PreviousTimeStep(variable)
+
+A structure to manually flag a variable in a model to use the value computed on the previous time-step. 
+This implies that the variable is not used to build the dependency graph because the dependency graph only 
+applies on the current time-step. This is used to avoid circular dependencies when a variable depends on itself.
+The value can be initialized in the Status if needed.
+"""
+struct PreviousTimeStep
+    variable::Symbol
+end
