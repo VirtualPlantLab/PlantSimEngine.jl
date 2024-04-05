9e3491c4bf0d76c9fdf823b348340733dd47b969
Rémi Vezy <VEZY@users.noreply.github.com>
2024-04-05 13:23:24 +0200

    Update save_results.jl
    Add tests to be sure that an organ for which users ask outputs exist in the MTG

--- src/mtg/save_results.jl
+++ src/mtg/save_results.jl
@@ -133,6 +133,18 @@ function pre_allocate_outputs(statuses, outs, nsteps; check=true)
 
     # Checking that variables in outputs exist in the statuses, and adding the :node variable:
     for (organ, vars) in outs_ # organ = "Leaf"; vars = outs_[organ]
+        if length(statuses[organ]) == 0
+            # The organ is not found in the mtg, we remove it from the outputs:
+            e = "You required outputs for organ $organ, but this organ is not found in the provided MTG."
+
+            if check
+                error(e)
+            else
+                @info e
+                delete!(outs_, organ)
+            end
+            continue
+        end
         if !all(i in collect(keys(statuses[organ][1])) for i in vars)
             not_in_statuses = (setdiff(vars, keys(statuses[organ][1]))...,)
             e = string(
