a9c62ba6f61c88cd84985371f5a1981d81389d2d
Rémi Vezy <VEZY@users.noreply.github.com>
2024-04-09 17:34:17 +0200

    Update MultiScaleModel.jl
    update doc

--- src/mtg/MultiScaleModel.jl
+++ src/mtg/MultiScaleModel.jl
@@ -13,12 +13,12 @@ The mapping can be of the form:
 
 1. `[:variable_name => "Plant"]`: We take one value from the Plant node
 2. `[:variable_name => ["Leaf"]]`: We take a vector of values from the Leaf nodes
-3. `[:variable_name => ["Leaf", "Internode"]]`:  We take a vector of values from the Leaf and Internode nodes
-4. `[:variable_name => "Plant" => :variable_name_in_plant_scale]`:  We take one value from another variable name in the Plant node
-5. `[:variable_name => ["Leaf" => :variable_name_1, "Internode" => :variable_name_2]]`:  We take a vector of values from the Leaf and Internode nodes with different names
-6. `[PreviousTimeStep(:variable_name) => ...]`:  We flag the variable to be initialized with the value from the previous time step, and we do not use it to build the dep graph
+3. `[:variable_name => ["Leaf", "Internode"]]`: We take a vector of values from the Leaf and Internode nodes
+4. `[:variable_name => "Plant" => :variable_name_in_plant_scale]`: We take one value from another variable name in the Plant node
+5. `[:variable_name => ["Leaf" => :variable_name_1, "Internode" => :variable_name_2]]`: We take a vector of values from the Leaf and Internode nodes with different names
+6. `[PreviousTimeStep(:variable_name) => ...]`: We flag the variable to be initialized with the value from the previous time step, and we do not use it to build the dep graph
 7. `[:variable_name => :variable_name_from_another_model]`: We take the value from another model at the same scale but rename it
-
+8. `[PreviousTimeStep(:variable_name),]`: We just flag the variable as a PreviousTimeStep to not use it to build the dep graph
 
 Details about the different forms:
 
@@ -40,6 +40,12 @@ when the variable name in the model is different from the variable name in the s
 6. The variable `variable_name` of the model uses the value computed on the previous time-step. This implies that the variable is not used to build the dependency graph
 because the dependency graph only applies on the current time-step. This is used to avoid circular dependencies when a variable depends on itself. The value can be initialized
 in the Status if needed.
+
+7. The variable `variable_name` of the model will be taken from another model at the same scale, but with another variable name.
+
+8. The variable `variable_name` of the model is just flagged as a PreviousTimeStep variable, so it is not used to build the dependency graph.
+
+
 
 Note that the mapping does not make any copy of the values, it only references them. This means that if the values are updated in the status
 of one node, they will be updated in the other nodes.
