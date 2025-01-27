aa750e00c2ed3777e9243f1a606cc5132d4a2262
github-action-benchmark <github@users.noreply.github.com>
2025-01-27 14:00:03 +0000

    add Julia benchmark result (julia) benchmark result for b071640d7a168947a9adea65e5cb3b24d5e5710c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1737986216666,
+  "lastUpdate": 1737986403274,
   "repoUrl": "https://github.com/VirtualPlantLab/PlantSimEngine.jl",
   "entries": {
     "Julia benchmark result": [
@@ -70,6 +70,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "bench/PBP",
             "value": 26579950,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=6290864\nallocs=101797\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "samuel.mackeown@cirad.fr",
+            "name": "Samuel-AMAP",
+            "username": "Samuel-amap"
+          },
+          "committer": {
+            "email": "samuel.mackeown@cirad.fr",
+            "name": "Samuel-AMAP",
+            "username": "Samuel-amap"
+          },
+          "distinct": true,
+          "id": "b071640d7a168947a9adea65e5cb3b24d5e5710c",
+          "message": "Reenable manual workflow triggering, and for pushes on the test branch",
+          "timestamp": "2025-01-27T14:46:51+01:00",
+          "tree_id": "c6ed8ae73f3ccdba13919347750c868668d907f4",
+          "url": "https://github.com/VirtualPlantLab/PlantSimEngine.jl/commit/b071640d7a168947a9adea65e5cb3b24d5e5710c"
+        },
+        "date": 1737986400526,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "bench/PSE",
+            "value": 29097660750,
+            "unit": "ns",
+            "extra": "gctime=271303958\nmemory=1570697760\nallocs=33400534\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "bench/PBP",
+            "value": 19249728.5,
             "unit": "ns",
             "extra": "gctime=0\nmemory=6290864\nallocs=101797\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
