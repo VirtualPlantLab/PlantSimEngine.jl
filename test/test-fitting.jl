b7f1d2f10070f829f2e9e5f6b2c89d21fed92c9f
Samuel-AMAP <samuel.mackeown@cirad.fr>
2025-03-21 14:49:34 +0100

    Fix test related to fitting (name export conflict, needs proper investigating at some point)

--- test/test-fitting.jl
+++ test/test-fitting.jl
@@ -4,11 +4,11 @@
     k = 0.6
     meteo = Atmosphere(T=20.0, Wind=1.0, P=101.3, Rh=0.65, Ri_PAR_f=300.0)
     m = ModelList(Beer(k), status=(LAI=2.0,))
-    outputs = run!(m, meteo)
+    outs = run!(m, meteo)
 
-    df = DataFrame(aPPFD=outputs[:aPPFD][1], LAI=m.status.LAI[1], Ri_PAR_f=meteo.Ri_PAR_f[1])
+    df = DataFrame(aPPFD=outs[:aPPFD][1], LAI=m.status.LAI[1], Ri_PAR_f=meteo.Ri_PAR_f[1])
 
-    k_fit = fit(Beer, df).k
+    k_fit = fit(PlantSimEngine.Examples.Beer, df).k
     @test k_fit == k
 end;
 
