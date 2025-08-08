5eaaeddc912f4fceaa6ce7ac99dd51606e58f409
Rémi Vezy <VEZY@users.noreply.github.com>
2025-08-08 16:41:49 +0200

    Fix error of a single Status for all timesteps (I introduced this in 60d448a8867b2410add00ee779e967e1d19d42e7)

--- src/mtg/save_results.jl
+++ src/mtg/save_results.jl
@@ -332,7 +332,8 @@ function pre_allocate_outputs(m::ModelList, outs, nsteps; type_promotion=nothing
     # default implicit behaviour, track everything
     if isempty(out_keys_requested)
         # We already have the status here, just repeating its value:
-        outputs_timestep = fill(NamedTuple(out_vars_all), nsteps)
+        @show out_vars_all
+        return TimeStepTable(fill(out_vars_all, nsteps))
     else
         unexpected_outputs = setdiff(out_keys_requested, keys(st))
 
@@ -353,9 +354,9 @@ function pre_allocate_outputs(m::ModelList, outs, nsteps; type_promotion=nothing
 
         out_defaults_requested = (out_vars_all[i] for i in out_keys_requested)
         out_vars_requested = (; zip(out_keys_requested, out_defaults_requested)...)
-        outputs_timestep = fill(out_vars_requested, nsteps)
     end
 
+    outputs_timestep = fill(out_vars_requested, nsteps)
     return TimeStepTable([Status(i) for i in outputs_timestep])
 end
 
